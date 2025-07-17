0cdebc4c587616f7a113a2527ff43e56e73f8321
Arul Thiru Raja <144304981+Arul-003@users.noreply.github.com>
2025-07-17 09:07:55 +0530

    Update ifelseif.js

--- ifelseif.js
+++ ifelseif.js
@@ -1,6 +1,6 @@
 for(let num=1;num<=20;num++)
     if(num % 5==0 )
-        console.log("the number is divisible by 5");
+        console.log("the number",num," is divisible by 5");
     else if(num%2==0)
         console.log("the num is even",+num);
     else(
